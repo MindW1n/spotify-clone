4debafc32457288bc1054be1a884cc3494f726f7
MindW1n <ivan.prosworker@gmail.com>
2024-02-25 19:13:27 +0300

    Update

--- components/primitives/Card.jsx
+++ components/primitives/Card.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link"
-export default function Card({image, label, description, href}) {
+export default async function Card({image, label, description, href}) {
 	return (
-		<Link href={"" && href} className="flex flex-col justify-between space-y-2 bg-neutral-800 w-44 h-64 rounded-lg p-4 transition-colors hover:bg-neutral-700 duration-300">
+		<Link href={"" && href} className="flex flex-col justify-between space-y-2 bg-neutral-850 w-44 h-64 rounded-xl p-4 transition-colors hover:bg-neutral-800 duration-300">
 			<div className="flex items-center h-full">
-				<img src={image} alt="label" className="rounded-md" />
+				<img src={image} alt={label} />
 			</div>
 			<div className="flex flex-col space-y-2">
 				<h3 className="text-sm font-bold truncate">{label}</h3>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-	'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-	'./components/**/*.{js,ts,jsx,tsx,mdx}',
-	'./app/**/*.{js,ts,jsx,tsx,mdx}'
-  ]
+	theme: {
+		extend: {
+			colors: {
+				"neutral-850": "rgb(30 30 30)"
+			}
+		}
+	},
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}'
+	]
 }
